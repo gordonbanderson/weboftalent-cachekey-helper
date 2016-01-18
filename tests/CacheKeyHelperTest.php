0149c04428446e8bb1b0691ee34ceb90fc909998
Gordon Anderson <gordon.b.anderson@gmail.com>
2016-01-18 21:14:32 +0700

    WIP: Tests shell

---
+++ tests/CacheKeyHelperTest.php
@@ -0,0 +1,24 @@
+<?php
+
+class CacheKeyHelperTest extends SapphireTest {
+	public function testCacheParamKey() {
+		$this->markTestSkipped('TODO');
+	}
+
+	public function testCacheKeyGetParam() {
+		$this->markTestSkipped('TODO');
+	}
+
+	public function testCacheKey() {
+		$this->markTestSkipped('TODO');
+	}
+
+	public function testCacheDataKey() {
+		$this->markTestSkipped('TODO');
+	}
+
+	public function testPrime_cache_keys() {
+		$this->markTestSkipped('TODO');
+	}
+
+}
