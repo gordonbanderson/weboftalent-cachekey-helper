1d09b5c2fb0b3e13d3b255252b4bf6106ccb6109
Gordon Anderson <gordon.b.anderson@gmail.com>
2018-05-06 00:02:28 +0700

    FIX: PSR2

--- tests/CacheKeyHelperTest.php
+++ tests/CacheKeyHelperTest.php
@@ -2,25 +2,30 @@
 
 use SilverStripe\Dev\SapphireTest;
 
-class CacheKeyHelperTest extends SapphireTest {
-	public function testCacheParamKey() {
-		$this->markTestSkipped('TODO');
-	}
+class CacheKeyHelperTest extends SapphireTest
+{
+    public function testCacheParamKey()
+    {
+        $this->markTestSkipped('TODO');
+    }
 
-	public function testCacheKeyGetParam() {
-		$this->markTestSkipped('TODO');
-	}
+    public function testCacheKeyGetParam()
+    {
+        $this->markTestSkipped('TODO');
+    }
 
-	public function testCacheKey() {
-		$this->markTestSkipped('TODO');
-	}
+    public function testCacheKey()
+    {
+        $this->markTestSkipped('TODO');
+    }
 
-	public function testCacheDataKey() {
-		$this->markTestSkipped('TODO');
-	}
+    public function testCacheDataKey()
+    {
+        $this->markTestSkipped('TODO');
+    }
 
-	public function testPrime_cache_keys() {
-		$this->markTestSkipped('TODO');
-	}
-
+    public function testPrime_cache_keys()
+    {
+        $this->markTestSkipped('TODO');
+    }
 }
