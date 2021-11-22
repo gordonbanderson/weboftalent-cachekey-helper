882118e920179db68ac1c34ff8467ed705af2807
Gordon Anderson <gordon.b.anderson@gmail.com>
2021-11-22 19:51:33 +0000

    WIP: Added test for cache key param

--- tests/CacheKeyHelperTest.php
+++ tests/CacheKeyHelperTest.php
@@ -4,18 +4,25 @@ declare(strict_types = 1);
 
 namespace WebOfTalent\Cache\Tests;
 
+use SilverStripe\CMS\Model\SiteTree;
 use SilverStripe\Dev\FunctionalTest;
-use SilverStripe\Dev\SapphireTest;
 
 class CacheKeyHelperTest extends FunctionalTest
 {
     public function testCacheParamKey(): void
     {
-        $page = $this->get('home/');
+        $homePage = SiteTree::get_by_id(1);
+        $response = $this->get('home/?x=40&y=52');
+        error_log(print_r($response, true));
 
         // Home page should load..
-        $this->assertEquals(200, $page->getStatusCode());
+        $this->assertEquals(200, $response->getStatusCode());
 
+        $this->assertEquals('_x_', $homePage->CacheParamKey('x'));
+        $this->assertEquals('_y_', $homePage->CacheParamKey('y'));
+
+        // this will still work even if not present in the URL
+        $this->assertEquals('_z_', $homePage->CacheParamKey('z'));
     }
 
 
